fc345391342f3fa89ddaf491d90ff3c38b04cf94
rabiilfarakh <rabiilfarakh2816@gmail.com>
2024-02-10 18:31:53 +0100

    front end

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -1,1 +1,10 @@
 <h1>admin</h1>
+<form method="POST" action="{{ route('logout') }}">
+    @csrf
+
+    <x-dropdown-link :href="route('logout')"
+            onclick="event.preventDefault();
+                        this.closest('form').submit();">
+        {{ __('Log Out') }}
+    </x-dropdown-link>
+</form>
