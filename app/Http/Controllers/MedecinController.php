28c63b3b7865c1911f31c4f0f37ffe24a57c1ae9
rabiilfarakh <rabiilfarakh2816@gmail.com>
2024-02-15 09:52:03 +0100

    add modification in dash medecin

--- app/Http/Controllers/MedecinController.php
+++ app/Http/Controllers/MedecinController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use  App\Models\Medecin;
+use App\Models\Specialite;
 class MedecinController extends Controller
 {
     public function showIndex(){
         return view('medecin.index');
     }
+    
     public function showProfile($id){
         $medecin = Medecin::findOrFail($id);
-    return view('medecin.profile', ['medecin' => $medecin]);
+        $specialite = Specialite::findOrFail($medecin->specialite_id);
+        return view('medecin.profile', compact('medecin','specialite'));
     }
+    
 }
